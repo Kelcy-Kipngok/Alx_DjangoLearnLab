44dcfc08aca6efe1ab14341cab9ad94900044a80
Kelcy <kelcykipngok@gmail.com>
2025-09-07 11:19:09 +0300

    Update and rename django-models/Introduction_to_Django/LibraryProject/LibraryProject/settings.py to django-models/Introduction_to_Django/LibraryProject/settings.py

--- django-models/Introduction_to_Django/LibraryProject/settings.py
+++ django-models/Introduction_to_Django/LibraryProject/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
@@ -120,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
