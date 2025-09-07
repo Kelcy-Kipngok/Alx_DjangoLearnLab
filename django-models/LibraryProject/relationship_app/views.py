913a69dddd80d72a5ead7112ef32870ab9ac8de2
Kelcy <kelcykipngok@gmail.com>
2025-09-07 10:55:10 +0300

    Update and rename django-models/relationship_app/views.py to django-models/LibraryProject/relationship_app/views.py

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import Book
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "list_books.html", {"books": books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
+
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+class CustomLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
