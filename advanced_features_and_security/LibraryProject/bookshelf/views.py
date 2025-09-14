bac1ccc6c99f7b982d951e794d5f4171e72dd66c
Kelcy <kelcykipngok@gmail.com>
2025-09-14 16:55:23 +0300

    Create views.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,37 @@
+# blog/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Article
+
+@permission_required("blog.can_view", raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, "blog/article_list.html", {"articles": articles})
+
+@permission_required("blog.can_create", raise_exception=True)
+def article_create(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        Article.objects.create(title=title, content=content, author=request.user)
+        return redirect("article_list")
+    return render(request, "blog/article_form.html")
+
+@permission_required("blog.can_edit", raise_exception=True)
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        article.title = request.POST.get("title")
+        article.content = request.POST.get("content")
+        article.save()
+        return redirect("article_list")
+    return render(request, "blog/article_form.html", {"article": article})
+
+@permission_required("blog.can_delete", raise_exception=True)
+def article_delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        article.delete()
+        return redirect("article_list")
+    return render(request, "blog/article_confirm_delete.html", {"article": article})
+v
