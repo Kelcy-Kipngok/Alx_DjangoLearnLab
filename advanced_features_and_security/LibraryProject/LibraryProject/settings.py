e832a607c73460b6f0c0f806d38ceaf085a536e3
Kelcy <kelcykipngok@gmail.com>
2025-09-14 17:00:20 +0300

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+ECURE_BROWSER_XSS_FILTER", "X_FRAME_OPTIONS", "SECURE_CONTENT_TYPE_NOSNIFF", "CSRF_COOKIE_SECURE", "SESSION_COOKIE_SECURE
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
