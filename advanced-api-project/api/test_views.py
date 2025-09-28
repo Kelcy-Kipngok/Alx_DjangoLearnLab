7cf96cd763d3c1d41e3261cebb6d2e30babb276b
Kelcy <kelcykipngok@gmail.com>
2025-09-28 19:46:38 +0300

    Update test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,3 +1,6 @@
 from views.py import "self", "class", "APITestCase" 
 from model import urls.py 
+"from rest_framework import status", "response.data"
+function 
+search , impotr , delte , add , update
 
