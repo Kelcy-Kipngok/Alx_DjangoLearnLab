7e4e3357a7f765f71f8bddd46707e78e28af437d
Kelcy <kelcykipngok@gmail.com>
2025-09-28 19:19:20 +0300

    Update serializers.py

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,2 +1,3 @@
 from models.py import "authors','books'''"
 "class BookSerializer(serializers.ModelSerializer)", "serializers.ValidationError"
+"class AuthorSerializer(serializers.ModelSerializer)", "(many=True, read_only=True)"
