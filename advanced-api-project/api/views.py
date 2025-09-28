1e8dd149c0cfe6759990f8e42e57d6ede51686d4
Kelcy <kelcykipngok@gmail.com>
2025-09-28 19:26:31 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
-
+"from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"
 # -----------------------------
 # LIST + CREATE
 # -----------------------------
@@ -39,5 +39,6 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
         return [permissions.AllowAny()]
 
 # Create your views here.
+
 
 
